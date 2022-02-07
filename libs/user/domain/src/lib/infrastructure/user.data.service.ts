a8fa4c4267fb88287ae72ed6b6347e243c7b8053
milan.dzenovljanovic <milan.dzenovljanovic@bitpanda.com>
2022-02-07 10:28:27 +0100

    user domain extends entity-list domain

--- libs/user/domain/src/lib/infrastructure/user.data.service.ts
+++ libs/user/domain/src/lib/infrastructure/user.data.service.ts
@@ -1,33 +1,23 @@
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Params } from '@angular/router';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 import { IUser, IUserListEntity } from '../entities/user';
-
-interface IResponse {
-  total_count: number;
-  items: IUserListEntity[];
-}
+import {
+  EntityListDataService,
+  EntityListType,
+} from '@github-search-ddd/shared/entity-list/domain';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserDataService {
+export class UserDataService extends EntityListDataService<
+  IUserListEntity,
+  IUser
+> {
   constructor(
-    @Inject('BASE_URL') private BASE_URL: string,
-    private _http: HttpClient
-  ) {}
-
-  public getSearchResult(param: Params): Observable<IResponse> {
-    const params = new HttpParams({
-      fromObject: param,
-    });
-
-    return this._http.get<IResponse>(this.BASE_URL + '/search/users', {params});
-  }
-
-  public getUserById(userId: string): Observable<IUser> {
-    return this._http.get<IUser>(this.BASE_URL + `/users/${userId}`);
+    @Inject('BASE_URL') protected override BASE_URL: string,
+    protected override _http: HttpClient
+  ) {
+    super(BASE_URL, _http, EntityListType.USERS);
   }
 }
